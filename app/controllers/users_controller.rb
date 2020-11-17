e738c2db1c68b5a03d3fa70bdd349177aedd401d
Landon Whitesides <whitesideslandon@gmail.com>
2020-11-17 16:05:13 -0700

    added ability to create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,20 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new()
     
+
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    #Why is this Necessary??
+    if @user.save
+    redirect_to users_path
+    else
+    render :new
+    end
   end
 
   def show
@@ -12,5 +25,11 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.new(user_params)
   end
+
+  def user_params
+    params.require(:user).permit(:full_name, :age, :gender, :alive)
+  end
+  
 end
